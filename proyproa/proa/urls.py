e65f32afaa580c3860ce06be3f94d082c25ad95d
FabrizioArnoletto <fearnolettodifilippo@escuelasproa.edu.ar>
2024-09-10 15:20:35 -0300

    links v2

--- proyproa/proa/urls.py
+++ proyproa/proa/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-
+    path('acerca_de/', views.about, name='acerca_de'),
+    path('eventos/', views.eventos, name='eventos'),
+    path('alumnos/', views.alumnos, name='alumnos'),
+    path('contactos/', views.contactos, name='contactos'),
 ]
--- proyproa/proa/views.py
+++ proyproa/proa/views.py
@@ -3,3 +3,15 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'index.html')
+
+def about(request):
+    return render(request,'about.html')
+
+def eventos(request):
+    return render(request,'eventos.html')
+
+def alumnos(request):
+    return render(request,'alumnos.html')
+
+def contactos(request):
+    return render(request,'contactos.html')
